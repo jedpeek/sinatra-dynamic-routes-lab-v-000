7980cbd9ecd003c407dc629f0b7a4774ff9891a0
Jed Peek <jedpeek@yahoo.com>
2018-07-23 22:19:57 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -28,7 +28,7 @@ class App < Sinatra::Base
     get '/:operation/:number1/:number2' do
       @num1 = params[:number1].to_i
       @num2 = params[:number2].to_i
-      @operator = params[:operation]
+      @operator = params[:operation].to_s
 
       if @operator == 'add'
         answer = (@num1 + @num2).to_s
