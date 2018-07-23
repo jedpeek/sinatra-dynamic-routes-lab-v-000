1ca92b681ae269a1435072c7a6e784c72d9e7836
Jed Peek <jedpeek@yahoo.com>
2018-07-23 22:21:13 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -25,22 +25,22 @@ class App < Sinatra::Base
       "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
     end
 
-    get '/:operation/:number1/:number2' do
-      @num1 = params[:number1].to_i
-      @num2 = params[:number2].to_i
-      @operator = params[:operation].to_s
+  get '/:operation/:number1/:number2' do
+    @num1 = params[:number1].to_i
+    @num2 = params[:number2].to_i
+    @operator = params[:operation].to_s
 
-      if @operator == 'add'
-        answer = (@num1 + @num2).to_s
-      elsif @operator == 'subtract'
-        answer = (@num1 - @num2).to_s
-      elsif @operator == 'multiply'
-        answer = (@num1 * @num2).to_s
-      elsif @operator == 'divide'
-        answer = (@num1/@num2).to_s
-      else
-        answer = 'Unable to perform this operation'
-      end
-      answer
+    if @operator == 'add'
+      answer = (@num1 + @num2).to_s
+    elsif @operator == 'subtract'
+      answer = (@num1 - @num2).to_s
+    elsif @operator == 'multiply'
+      answer = (@num1 * @num2).to_s
+    elsif @operator == 'divide'
+      answer = (@num1/@num2).to_s
+    else
+      answer = 'Unable to perform this operation'
+    end
+    answer
   end
 end
